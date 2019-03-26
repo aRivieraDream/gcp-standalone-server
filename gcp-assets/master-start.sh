9b229e19362905d2b23c9e2b0606e39af2eaf189
Pierce <808pierce@gmail.com>
2019-03-26 11:52:27 -0700

    fix newline formatting in master-start.sh

--- gcp-assets/master-start.sh
+++ gcp-assets/master-start.sh
@@ -2,9 +2,9 @@
 
 gcloud deployment-manager deployments create tableau-standalone-server --config standalone-tableau-server.yaml
 wait
-printf '\n\n-----------\nThe VM has finished deploying but Tableau Server is still being installed ...'
+printf '\n\n-----------\nThe VM has finished deploying but Tableau Server is still being installed ...\n'
 IP=`gcloud compute instances describe tableau-server --zone=us-west1-a | grep natIP | awk '/natIP:/{print $NF}'`
-printf "Tableau Server will take ~15 minutes to install. "
-printf "Once the initial setup is complete (~5 minutes) you can check the progress of the installation in your web browser. "
+printf "Tableau Server will take ~15 minutes to install.\n "
+printf "Once the initial setup is complete (~5 minutes) you can check the progress of the installation in your web browser. \n"
 printf "To check the progress, visit: \n $IP:8850 \n and login using your credentials.\n\n"
-printf "Once installation is finished, you can visit your server at: \n$IP. Login credentials can be found in your secrets file."
+printf "Once installation is finished, you can visit your server at: \n$IP. \nLogin credentials can be found in your secrets file."
