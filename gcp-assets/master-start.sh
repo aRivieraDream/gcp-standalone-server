26ed55c6db883be2feae06a60b77dbec132691e0
Pierce <808pierce@gmail.com>
2019-03-26 10:40:00 -0700

    fix newline character in master-start

--- gcp-assets/master-start.sh
+++ gcp-assets/master-start.sh
@@ -2,9 +2,9 @@
 
 gcloud deployment-manager deployments create tableau-standalone-server --config standalone-tableau-server.yaml
 wait
-echo 'The VM has finished deploying but Tableau Server is still being installed ...'
+printf '\n\n-----------\nThe VM has finished deploying but Tableau Server is still being installed ...'
 IP=`gcloud compute instances describe tableau-server --zone=us-west1-a | grep natIP | awk '/natIP:/{print $NF}'`
-echo "Tableau Server will take ~15 minutes to install. "
-echo "Once the initial setup is complete (~5 minutes) you can check the progress of the installation in your web browser. "
-echo "To check the progress, visit: /n $IP:8850 /n and login using your credentials."
-echo "Once installation is finished, you can visit your server at $IP. Login credentials can be found in your secrets file."
+printf "Tableau Server will take ~15 minutes to install. "
+printf "Once the initial setup is complete (~5 minutes) you can check the progress of the installation in your web browser. "
+printf "To check the progress, visit: \n $IP:8850 \n and login using your credentials.\n\n"
+printf "Once installation is finished, you can visit your server at: \n$IP. Login credentials can be found in your secrets file."
