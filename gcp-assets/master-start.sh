ee61b5887b3ec7012cf039a93763c6d405b9bced
Pierce <808pierce@gmail.com>
2019-05-06 13:41:17 -0700

    update formatting in master-start output

--- gcp-assets/master-start.sh
+++ gcp-assets/master-start.sh
@@ -4,7 +4,7 @@ gcloud deployment-manager deployments create tableau-standalone-server --config 
 wait
 printf '\n\n-----------\nThe VM has finished deploying but Tableau Server is still being installed ...\n'
 IP=`gcloud compute instances describe tableau-server --zone=us-west1-a | grep natIP | awk '/natIP:/{print $NF}'`
-printf "Tableau Server will take ~15 minutes to install.\n "
-printf "Once the initial setup is complete (~5 minutes) you can check the progress of the installation in your web browser. \n"
-printf "To check the progress, visit: \n $IP:8850 \n and login using your credentials.\n\n"
-printf "Once installation is finished, you can visit your server at: \n$IP. \nLogin credentials can be found in your secrets file."
+printf "Tableau Server will take ~10 minutes to install.\n "
+printf "Once the initial setup is complete (~3 minutes) you can check the progress of the installation in your web browser. \n"
+printf "To check the progress, visit: \n https://$IP:8850 \n and login using your credentials. Note that this IP address has no SSL certificate, so a modern browser may warn you of a security risk. \n\n"
+printf "Once installation is finished, you can visit your server at: \n$IP \nLogin credentials can be found in your secrets file."
